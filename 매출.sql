91fe0ae015c02d9c85d8722223bd696c5d2d7167
jinlove323 <56267813+jinlove323@users.noreply.github.com>
2020-08-25 16:20:20 +0900

    Update 매출.sql

--- 매출.sql
+++ 매출.sql
@@ -183,6 +183,9 @@ VALUES
 
 
 
+
+
+
 --ABC 분석
 with temp as 
 (select category
@@ -197,3 +200,71 @@ from temp)
 select *
 	 , case when com_ration 
 from temp2
+
+
+-- 팬 차트로 상품의 매출 증가율 확인하기
+
+DROP TABLE IF EXISTS purchase_detail_log;
+CREATE TABLE purchase_detail_log(
+    dt           varchar(255)
+  , order_id     integer
+  , user_id      varchar(255)
+  , item_id      varchar(255)
+  , price        integer
+  , category     varchar(255)
+  , sub_category varchar(255)
+);
+
+INSERT INTO purchase_detail_log
+VALUES
+    ('2017-01-18', 48291, 'usr33395', 'lad533', 37300,  'ladys_fashion', 'bag')
+  , ('2017-01-18', 48291, 'usr33395', 'lad329', 97300,  'ladys_fashion', 'jacket')
+  , ('2017-01-18', 48291, 'usr33395', 'lad102', 114600, 'ladys_fashion', 'jacket')
+  , ('2017-01-18', 48291, 'usr33395', 'lad886', 33300,  'ladys_fashion', 'bag')
+  , ('2017-01-18', 48292, 'usr52832', 'dvd871', 32800,  'dvd'          , 'documentary')
+  , ('2017-01-18', 48292, 'usr52832', 'gam167', 26000,  'game'         , 'accessories')
+  , ('2017-01-18', 48292, 'usr52832', 'lad289', 57300,  'ladys_fashion', 'bag')
+  , ('2017-01-18', 48293, 'usr28891', 'out977', 28600,  'outdoor'      , 'camp')
+  , ('2017-01-18', 48293, 'usr28891', 'boo256', 22500,  'book'         , 'business')
+  , ('2017-01-18', 48293, 'usr28891', 'lad125', 61500,  'ladys_fashion', 'jacket')
+  , ('2017-01-18', 48294, 'usr33604', 'mem233', 116300, 'mens_fashion' , 'jacket')
+  , ('2017-01-18', 48294, 'usr33604', 'cd477' , 25800,  'cd'           , 'classic')
+  , ('2017-01-18', 48294, 'usr33604', 'boo468', 31000,  'book'         , 'business')
+  , ('2017-01-18', 48294, 'usr33604', 'foo402', 48700,  'food'         , 'meats')
+  , ('2017-01-18', 48295, 'usr38013', 'foo134', 32000,  'food'         , 'fish')
+  , ('2017-01-18', 48295, 'usr38013', 'lad147', 96100,  'ladys_fashion', 'jacket')
+  ,    ('2017-02-18', 48291, 'usr33395', 'lad533', 45000,  'ladys_fashion', 'bag')
+  , ('2017-02-18', 48291, 'usr33395', 'lad329', 99000,  'ladys_fashion', 'jacket')
+  , ('2017-02-18', 48291, 'usr33395', 'lad102', 150000, 'ladys_fashion', 'jacket')
+  , ('2017-02-18', 48291, 'usr33395', 'lad886', 40000,  'ladys_fashion', 'bag')
+  , ('2017-02-18', 48292, 'usr52832', 'dvd871', 71000,  'dvd'          , 'documentary')
+  , ('2017-02-18', 48292, 'usr52832', 'gam167', 30000,  'game'         , 'accessories')
+  , ('2017-02-18', 48292, 'usr52832', 'lad289', 59300,  'ladys_fashion', 'bag')
+  , ('2017-02-18', 48293, 'usr28891', 'out977', 59900,  'outdoor'      , 'camp')
+  , ('2017-02-18', 48293, 'usr28891', 'boo256', 33400,  'book'         , 'business')
+  , ('2017-02-18', 48293, 'usr28891', 'lad125', 67700,  'ladys_fashion', 'jacket')
+  , ('2017-02-18', 48294, 'usr33604', 'mem233', 117600, 'mens_fashion' , 'jacket')
+  , ('2017-02-18', 48294, 'usr33604', 'cd477' , 27800,  'cd'           , 'classic')
+  , ('2017-02-18', 48294, 'usr33604', 'boo468', 37000,  'book'         , 'business')
+  , ('2017-02-18', 48294, 'usr33604', 'foo402', 49900,  'food'         , 'meats')
+  , ('2017-02-18', 48295, 'usr38013', 'foo134', 37000,  'food'         , 'fish')
+  , ('2017-02-18', 48295, 'usr38013', 'lad147', 99700,  'ladys_fashion', 'jacket')
+ ;
+
+select * 
+from purchase_detail_log a
+
+with temp as 
+(select substr(dt,1,7) as year_month
+	 , 	category 
+	 ,  sum(price) as amt
+from purchase_detail_log a
+group by 1,2 
+order by 1,2)
+select *
+	 , first_value(amt) over (partition by category order by year_month,category rows unbounded preceding ) as base_amt
+	 , round((amt/first_value(amt) over (partition by category order by year_month,category rows unbounded preceding ))*100.0,2) as rate
+from temp 
+
+
+
